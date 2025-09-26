91f0d1c404ca8a5bb7b2339bcb1a88097a0e0303
jayanthan s <jayanthansathiyapal@gmail.com>
2025-09-27 00:40:41 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Movie-search/'   // <-- replace with your repo name
+  base: '/Movie-search/'
 });
